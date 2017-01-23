221513ca1d005fad24ba0436ef9af44da8231e7a
Minh Phuong and Sara Frandsen <minh+sara@>
2017-01-23 10:30:53 -0800

    Add user interface in JS

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,5 @@
 //BUSINESS LOGIC
-function data(Name,Thing1,Thing2,Thing3,Thing4,Thing5) {
+function List(Name,Thing1,Thing2,Thing3,Thing4,Thing5) {
   this.Name = Name;
   this.Thing1 = Thing1;
   this.Thing2 = Thing2;
@@ -13,7 +13,23 @@ $("document").ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
+    var fullName = $("input#name").val();
+    var firstThing = $("input#thing1").val();
+    var secondThing = $("input#thing2").val();
+    var thirdThing = $("input#thing3").val();
+    var fourthThing = $("input#thing4").val();
+    var fifthThing = $("input#thing5").val();
 
+    var list = new List(fullName, firstThing, secondThing, thirdThing, fourthThing, fifthThing)
 
+    $("h3.yourname").text(list.Name);
+    $("h3.yourname").last().click(function() {
+      $("#results").show();
+      $("ul#todolist").append("<li>" + list.Thing1 + "</li>")
+      $("ul#todolist").append("<li>" + list.Thing2 + "</li>")
+      $("ul#todolist").append("<li>" + list.Thing3 + "</li>")
+      $("ul#todolist").append("<li>" + list.Thing4 + "</li>")
+      $("ul#todolist").append("<li>" + list.Thing5 + "</li>")
+    });
   });
 });
