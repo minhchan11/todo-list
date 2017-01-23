1de1b12ded3262c9917c5fe4ea151127435d7d7a
Minh Phuong and Sara Frandsen <minh+sara@>
2017-01-23 10:16:01 -0800

    create basic HTML and JS

---
+++ JS/scripts.js
@@ -0,0 +1,19 @@
+//BUSINESS LOGIC
+function data(Name,Thing1,Thing2,Thing3,Thing4,Thing5) {
+  this.Name = Name;
+  this.Thing1 = Thing1;
+  this.Thing2 = Thing2;
+  this.Thing3 = Thing3;
+  this.Thing4 = Thing4;
+  this.Thing5 = Thing5;
+}
+
+//USER INTERFACE
+$("document").ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+
+
+
+  });
+});
