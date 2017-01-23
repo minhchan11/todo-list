c90358533e42cc0d33dc6f950f2e371722ca87a4
Minh Phuong and Sara Frandsen <minh+sara@>
2017-01-23 11:59:35 -0800

    add JS

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,36 +1,44 @@
 //BUSINESS LOGIC
-function List(Name,Thing1,Thing2,Thing3,Thing4,Thing5) {
-  this.Name = Name;
-  this.Thing1 = Thing1;
-  this.Thing2 = Thing2;
-  this.Thing3 = Thing3;
-  this.Thing4 = Thing4;
-  this.Thing5 = Thing5;
+
+//this is our actual list
+var toDoList = []
+
+//this defines each of our tasks
+function Task(name, priority, due, assigned) {
+  this.fullname = name;
+  this.priority = priority;
+  this.due = due;
+  this.assigned = assigned;
 }
+
+Task.prototype.addToList = function() {
+ toDoList.push(this);
+}
+
+Task.prototype.crossOut = function() {
+  $(this).toggleClass("cross")
+}
+
 
 //USER INTERFACE
 $("document").ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
-    var fullName = $("input#name").val();
-    var firstThing = $("input#thing1").val();
-    var secondThing = $("input#thing2").val();
-    var thirdThing = $("input#thing3").val();
-    var fourthThing = $("input#thing4").val();
-    var fifthThing = $("input#thing5").val();
+    var Name = $("input#name").val();
+    var Priority = $("input#priority").val();
+    var Due = $("input#due").val();
+    var Assigned = $("input#assigned").val();
 
-    var list = new List(fullName, firstThing, secondThing, thirdThing, fourthThing, fifthThing)
+    var individualTask = new Task(Name, Priority, Due, Assigned);
 
-    $("h3.yourname").text(list.Name);
-    $("h3.yourname").last().click(function() {
-      $("#results").show();
-      $("ul#todolist").append("<li>" + list.Thing1 + "</li>")
-      $("ul#todolist").append("<li>" + list.Thing2 + "</li>")
-      $("ul#todolist").append("<li>" + list.Thing3 + "</li>")
-      $("ul#todolist").append("<li>" + list.Thing4 + "</li>")
-      $("ul#todolist").append("<li>" + list.Thing5 + "</li>")
-      $("li").click(function() {$(this).toggleClass("cross")});
-    });
+    individualTask.addToList();
+
+    $("h3.taskName").text(individualTask.fullname);
+    $("#results").show();
+    $("ul#priority-toDo").append("<li>" + individualTask.priority + "</li>")
+    $("ul#dueDate-toDo").append("<li>" + individualTask.due + "</li>")
+    $("ul#assigned-toDo").append("<li>" + individualTask.assigned + "</li>")
+      // $("li").click(function() {$(this).toggleClass("cross")});
   });
 });
