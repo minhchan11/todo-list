fde665cceb99f77a84f5c338011ceed3a2a949a2
Minh Phuong and Sara Frandsen <minh+sara@>
2017-01-23 13:43:03 -0800

    create function in JS to generate results

--- JS/scripts.js
+++ JS/scripts.js
@@ -15,10 +15,15 @@ Task.prototype.addToList = function() {
  toDoList.push(this);
 }
 
-Task.prototype.crossOut = function() {
-  $(this).toggleClass("cross")
+function createList() {
+  for (var index = 0; index < toDoList.length; index++) {
+  console.log(toDoList[index])
+  $("#results").append("<h2>" + toDoList[index].fullname + "</h2>")
+  $("#results").append("<li>" + toDoList[index].priority + "</li>")
+  $("#results").append("<li>" + toDoList[index].due + "</li>")
+  $("#results").append("<li>" + toDoList[index].assigned + "</li>");
+  return true }
 }
-
 
 //USER INTERFACE
 $("document").ready(function() {
@@ -31,14 +36,10 @@ $("document").ready(function() {
     var Assigned = $("input#assigned").val();
 
     var individualTask = new Task(Name, Priority, Due, Assigned);
+    $("#results").show();
 
     individualTask.addToList();
+    createList();
 
-    $("h3.taskName").text(individualTask.fullname);
-    $("#results").show();
-    $("ul#priority-toDo").append("<li>" + individualTask.priority + "</li>")
-    $("ul#dueDate-toDo").append("<li>" + individualTask.due + "</li>")
-    $("ul#assigned-toDo").append("<li>" + individualTask.assigned + "</li>")
-      // $("li").click(function() {$(this).toggleClass("cross")});
   });
 });
